e3e0007b00df4d1a988e933aefd0d5804c84bfa2
Patricia Faustino <patriciafaustinobr@gmail.com>
2023-02-24 21:35:48 -0300

    feat: adding ansible configuration and uploading index.html file

--- main.tf
+++ main.tf
@@ -18,7 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-00eeedc4036573771"
   instance_type = "t2.micro"
   key_name = "aws-ohiio"
+#  user_data_replace_on_change = true
+#  user_data = <<-EOF
+#                #!/bin/bash
+#                cd /home/ubuntu
+#                echo "<h1>Mensagem a ser mostrada</h1>" > index.html
+#                nohup busybox httpd -f -p 8080 &
+#                EOF
   tags = {
-    Name = "Primeira instância"
+    Name = "Teste aws instancia"
   }
 }
